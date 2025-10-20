6d5de23c8534842628f783079a6d844b53ae4ea7
Ryan Gerstenkorn <me@ryanjarv.sh>
2025-10-20 16:25:18 -0700

    Revert to older download method with a broader time range

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 rm -rf ./docs
-docker run -v "${PWD}/site:/site" -w /site wayback-downloader 'http://secretgardens.com' --all-timestamps
+docker run -v "${PWD}/site:/site" -w /site wayback-downloader 'http://secretgardens.com' --from 20000207045252 --to 20260207045252
 mkdir -p ./docs && cp -a site/websites/secretgardens.com/* ./docs
 echo -n 'secretgardens.ryanjarv.sh' > docs/CNAME
