d2cf653d92d60030d6d241b66f437cbdbc38f53a
Ryan Gerstenkorn <me@ryanjarv.sh>
2025-10-20 16:27:58 -0700

    No need to remove files

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-rm -rf ./docs
 docker run -v "${PWD}/site:/site" -w /site wayback-downloader 'http://secretgardens.com' --from 20000207045252 --to 20260207045252
-mkdir -p ./docs && cp -a site/websites/secretgardens.com/* ./docs
+cp -a site/websites/secretgardens.com/* ./docs/
 echo -n 'secretgardens.ryanjarv.sh' > docs/CNAME
